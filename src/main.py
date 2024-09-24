6721ebb3c134e99d7562787248d5a02398198c8f
OmarEP <omrspz@gmail.com>
2024-09-24 16:20:04 -0400

    add html/parent nodes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from textnode import TextNode
+from htmlnode import HTMLNode
 
 def main():
     node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(node)
+    html_node = HTMLNode(None, None, None, {
+            "href": "https://www.google.com", 
+            "target": "_blank",
+    })
+    print(html_node.props_to_html())
 
 
 if __name__ == "__main__":
