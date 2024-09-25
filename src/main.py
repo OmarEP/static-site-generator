46295b3efc2b1fb621b031b1c52e2da8abdc9274
OmarEP <omrspz@gmail.com>
2024-09-25 12:41:55 -0400

    add inline_markdown extract split images and links

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 from textnode import TextNode
 from htmlnode import HTMLNode
+from inline_markdown import *
 
 def main():
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    html_node = HTMLNode(None, None, None, {
-            "href": "https://www.google.com", 
-            "target": "_blank",
-    })
-    print(html_node.props_to_html())
+    node = TextNode(
+    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+    text_type_text,
+    )
+    new_nodes = split_nodes_link([node])
+    
+
 
 
 if __name__ == "__main__":
