679d807a5418c4f27573bc05e447fd0a7d82d09f
OmarEP <omrspz@gmail.com>
2024-09-27 09:11:03 -0400

    add static file copyig

--- src/main.py
+++ src/main.py
@@ -1,26 +1,19 @@
-from textnode import TextNode
-from htmlnode import HTMLNode
-from inline_markdown import *
-from markdown_blocks import(
-    markdown_to_blocks,
-    block_to_block_type,
-    markdown_to_html_node,
-)
+import os 
+import shutil
+
+from copystatic import copy_files_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 def main():
-    md = """
-- This is a list
-- with items
-- and *more* items
-
-1. This is an `ordered` list
-2. with items
-3. and more items
-
-"""
-    print(markdown_to_html_node(md).to_html())
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+    
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 if __name__ == "__main__":
     main()
 
-'<div><ol><li>This is a list</li><li>with items</li><li>and <i>more</i> items</li></ol><ol><li>This is an <code>ordered</code> list</li><li>with items</li><li>and more items</li></ol></div>'
